7f23d71cd50e6cbdd8eeba238962b525923e27d9
agungw132 <agung.wahyudi@gmail.com>
2019-07-26 04:25:42 +0200

    initial version

---
+++ @def.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+clear
+word=`curl --get --include "https://wordsapiv1.p.rapidapi.com/words/$1/definitions" \
+  -H 'X-RapidAPI-Host: wordsapiv1.p.rapidapi.com' \
+  -H 'X-RapidAPI-Key: [key]' | \
+grep "{"`
+echo $word
+mosquitto_pub -h [host] -u [user] -P [password] -t tele/home-mn356/dictionary/definitions/$1 -m "$word"
+
+
